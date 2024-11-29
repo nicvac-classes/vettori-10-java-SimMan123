4488db57402d2bad239b84dffe37811e5862ce8a
SimMan123 <simone.manzari2@marconibari.edu.it>
2024-11-29 10:24:08 +0000

    vector10

--- Esercizio.java
+++ Esercizio.java
@@ -8,19 +8,34 @@ class Esercizio {
     // Il programma parte con una chiamata a main().
     public static void main(String args[])
     {
-        //Variabili del programma
-        String nome;
-
-        //Creo l'oggetto in per l'input da tastiera
         Scanner in = new Scanner( System.in );
+        int i,n,m;
+        System.out.print("inserire n");
+        n=in.nextInt;
+        int[] c = new int[n];
+        int[] v = new int[n];
+        String[] nomi = new String[n];
+        m=0;
+        for (i=0;i<n;i++) {
+            System.out.print("inserire nomi " + i + "^ studente");
+            nomi[i]= in.nextLine;
+            c[i]=random(10);
+            v[i]=ramdom(10);
+            m=m+v[i];
+        }
+        m=m/n;
+        for (i=0;i<n;i++) {
+            if (v[i])>m {
+                System.out.print(nomi[i] + " ha ottenuto un voto superiore alla media ");
+                System.out.print("voto : " + v[i]);
+            }
+            if (c[i]>=6) {
+                System.out.print(c[i] + " ha ottenuto crediti superiore alla media ");
+                System.out.print("crediti " +  c[i]);
+            }
+        }
 
-        //Leggo l'input da tastiera
-        System.out.print("Inserisci il tuo nome: ");
-        nome = in.nextLine();
-
-        //Output del nome acquisito da tastiera
-        System.out.println("Ciao "+nome+"!");
+    
     }
 }
 
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
